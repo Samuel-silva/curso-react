8980a40b639937d222e1bae0456b291d77277dc7
Samuel Barbosa <samuca192silva@hotmail.com>
2023-04-13 17:11:39 -0300

    feat - useContext

--- hooks/src/views/App.jsx
+++ hooks/src/views/App.jsx
@@ -1,17 +1,26 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
+import { DataContext, data } from '../data/DataContext'
 
 import Menu from '../components/layout/Menu'
 import Content from '../components/layout/Content'
+import { useState } from 'react'
+import Store from '../data/Store'
 
 const App = props => {
+  const [state, setState] = useState(data)
+
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Menu />
-        <Content />
-      </BrowserRouter>
-    </div>
+    <Store>
+      <DataContext.Provider value={{state, setState}}>
+        <div className="App">
+          <BrowserRouter>
+            <Menu />
+            <Content />
+          </BrowserRouter>
+        </div>
+      </DataContext.Provider>
+    </Store>
   )
 }
 
