ff148d2b36b2eb2d59b41042dca1b2aacd12ca7b
Samuel Barbosa <samuca192silva@hotmail.com>
2023-06-07 15:43:10 -0300

    feat - Alterando o estado

---
+++ redux-simples/src/store/actions/numeros.js
@@ -0,0 +1,15 @@
+function alterarNumeroMinimo(novoNumero) {
+  return {
+    type: 'NUM_MIN_ALTERADO',
+    payload: novoNumero
+  }
+}
+
+function alterarNumeroMaximo(novoNumero) {
+  return {
+    type: 'NUM_MAX_ALTERADO',
+    payload: novoNumero
+  }
+}
+
+export { alterarNumeroMinimo, alterarNumeroMaximo };
--- redux-simples/src/store/storeConfig.js
+++ redux-simples/src/store/storeConfig.js
@@ -2,9 +2,22 @@ import { createStore, combineReducers } from 'redux'
 
 const reducers = combineReducers({
   numeros: function(state, action) {
-    return {
-      min: 1,
-      max: 10,
+    switch (action.type) {
+      case 'NUM_MIN_ALTERADO':
+        return {
+          ...state,
+          min: action.payload
+        }
+      case 'NUM_MAX_ALTERADO':
+          return {
+            ...state,
+            max: action.payload
+          }
+      default:
+        return {
+          min: 1,
+          max: 20,
+        }
     }
   }
 });
